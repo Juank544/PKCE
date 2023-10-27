3a0759d2e9a8d6623e385ac60134f784deee59e8
juangilv <juan.gilv@perficient.com>
2023-10-27 09:43:15 -0500

    initial commit

---
+++ src/test/java/co/com/perficient/pkce/PkceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.perficient.pkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
