8dc352b0ab20ce8605df7036b360260f5bb3c191
Juank544 <camilo.gv@live.com>
2023-11-21 09:01:35 -0500

    feat(spa): services for keycloak-pkce auth

--- src/main/resources/spa/src/app/app.component.ts
+++ src/main/resources/spa/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  public isLoggedIn = false;
+
+  constructor(private readonly keycloak: KeycloakService) { }
+
+  public async ngOnInit() {
+    this.isLoggedIn = await this.keycloak.isLoggedIn();
+  }
+
+  public login() {
+    this.keycloak.login();
+  }
+
+  public logout() {
+    let redirectURI: string = "http://localhost:4200/dashboard";
+    this.keycloak.logout(redirectURI);
+  }
 }
--- src/main/resources/spa/src/app/app.module.ts
+++ src/main/resources/spa/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,13 +11,30 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8180',
+        realm: 'pkce',
+        clientId: 'spa-pkce'
+      },
+      initOptions: {
+        pkceMethod: 'S256',
+        redirectUri: 'http://localhost:4200/dashboard'
+      }, loadUserProfileAtStartUp: false
+    });
+}
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    KeycloakAngularModule
   ],
   declarations: [
     AppComponent,
@@ -26,6 +43,14 @@ import { MessagesComponent } from './messages/messages.component';
     HeroDetailComponent,
     MessagesComponent,
     HeroSearchComponent
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
   ],
   bootstrap: [ AppComponent ]
 })
